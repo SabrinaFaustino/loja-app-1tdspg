2cc3f22f389347a2d8b3714ad4c17ade86b463ce
SabrinaFaustino <rm99570@fiap.com.br>
2023-11-01 09:00:12 -0300

    feat: dynamic route with id

--- src/app/api/base/base-users/[id]/route.jsx
+++ src/app/api/base/base-users/[id]/route.jsx
@@ -2,6 +2,26 @@ import {promises as fs} from "fs";
 import { NextResponse } from "next/server";
 
 export async function GET(request,{params}) {
+
   const file = await fs.readFile(process.cwd() + "/src/app/api/base/db.json", 'utf8');
-  return NextResponse.json(JSON.parse(file));
+
+  //Extraindo a lista de usuários do arquivo JSON:
+  const lista = await JSON.parse(file);
+
+  const id = params.id;
+
+  if(id > 0 && id <= lista.usuarios.length){
+    return NextResponse.json(lista.usuarios.find((user)=> user.id == id));
+  }else{
+    return id == 0 ? NextResponse.json(lista.usuarios) : NextResponse.redirect("http://localhost:3000/error")
+  }
+}
+
+export async function POST(request, response){
+  const file = await fs.readFile(process.cwd() + "/src/app/api/base/db.json", 'utf8');
+
+  //Extraindo a lista de usuários do arquivo JSON:
+  const lista = await JSON.parse(file);
+
+  const id = params.id;
 }
